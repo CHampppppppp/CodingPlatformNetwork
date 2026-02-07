c4db20b4c711e64a05e24ff7dbf42d1f7e4e39bd
tkssoul <2495830486@qq.com>
2026-02-08 04:43:19 +0800

    chore(test): 添加端到端测试配置

---
+++ playwright.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  webServer: [
+    {
+      command: "cd backend && pnpm run start:dev",
+      port: 3001,
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: "cd frontend && pnpm run dev",
+      port: 3000,
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
+  reporter: [["html"]],
+  use: {
+    baseURL: "http://localhost:3000",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { browserName: "chromium" },
+    },
+    {
+      name: "firefox",
+      use: { browserName: "firefox" },
+    },
+    {
+      name: "webkit",
+      use: { browserName: "webkit" },
+    },
+  ],
+});
