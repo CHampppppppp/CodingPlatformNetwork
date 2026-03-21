446df47b73a87ccbd41dc7928446524b1c6c338d
tkssoul <2495830486@qq.com>
2026-03-22 03:14:40 +0800

    style: simplify index.tsx by removing ErrorBoundary wrapper

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import ErrorBoundary from './ErrorBoundary';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
@@ -11,8 +10,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
